448d528a8e966a445c6ba84e25747c3541a031ce
JuanVizcaya <juanj.vizcaya@gmail.com>
2021-02-07 21:05:23 -0600

    prod intento 6, DB PASSWORD

--- tasks/settings/prod.py
+++ tasks/settings/prod.py
@@ -20,6 +20,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('PROD_DB_NAME'),
         'USER': env('PROD_DB_USER'),
+        'PASSWORD': env('PROD_DB_PASSWORD'),
         'HOST': env('PROD_DB_HOST'),
         'PORT': env('PROD_DB_PORT')
     }
